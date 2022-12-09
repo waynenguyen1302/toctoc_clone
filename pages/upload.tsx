7d580ee344c83538e09fe88242069a77141ac135
waynenguyen1302 <111101794+waynenguyen1302@users.noreply.github.com>
2022-12-09 13:33:55 -0800

    change next config

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -10,7 +10,7 @@ import {topics} from '../utils/constants';
 import { BASE_URL } from '../utils';
 
 
-const upload = () => {
+const Upload = () => {
 
     const [isLoading, setIsLoading] = useState(false);
     const [videoAsset, setVideoAsset] = useState<SanityAssetDocument | undefined>();
@@ -184,4 +184,4 @@ const upload = () => {
   )
 }
 
-export default upload
+export default Upload
