85647e33b87638fc918da9d6a45405b53019e9ca
waynenguyen1302 <111101794+waynenguyen1302@users.noreply.github.com>
2022-12-10 13:08:55 -0800

    change next config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,41 @@
-import type { NextPage } from "next";
+import React from 'react';
 import axios from 'axios';
-import {Video} from '../types';
-import VideoCard from "../components/VideoCard";
-import NoResults from "../components/NoResults";
-import { BASE_URL } from "../utils";
+
+import VideoCard from '../components/VideoCard';
+import { BASE_URL } from '../utils';
+import { Video } from '../types';
+import NoResults from '../components/NoResults';
 
 interface IProps {
-  videos: Video[],
+  videos: Video[];
 }
 
-const Home = ( {videos}: IProps) => {
-  console.log(videos)
+const Home = ({ videos }: IProps) => {
   return (
-    <div className="flex flex-col gap-10 videos h-full">
-      {videos.length ? (
-        videos.map((video: Video) => (
-          <VideoCard post={video} key={video._id} />
-        ))
-      ) : (
-        <NoResults text = {'No Video'} />
-      )}
+    <div className='flex flex-col gap-10 videos h-full'>
+      {videos.length 
+        ? videos?.map((video: Video) => (
+          <VideoCard post={video} isShowingOnHome key={video._id} />
+        )) 
+        : <NoResults text={`No Videos`} />}
     </div>
-  )
-}
+  );
+};
 
-export const getServerSideProps = async ({query: { topic }} : {query : { topic : string}}) => {
-  let response = null;
-  
-  if(topic) {    
-    // filter by topics
+export default Home;
+
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string };
+}) => {
+  let response = await axios.get(`${BASE_URL}/api/post`);
+
+  if(topic) {
     response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
-  } else {
-    response = await axios.get(`${BASE_URL}/api/post`);
   }
   
-
   return {
-    props: {
-      videos: response.data
-    }
-  }
-}
-export default Home
+    props: { videos: response.data },
+  };
+};
